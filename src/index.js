a316ff9468e9c5fc4add0c77c27c481440eea327
Lauri Peltonen <lpeltonen@nvidia.com>
2017-05-12 19:07:44 +0300

    Hammer works

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import GameState from 'states/GameState';
 class Game extends Phaser.Game {
 
   constructor() {
-    super(1280, 720, Phaser.AUTO, 'content', null);
-      this.state.add('GameState', GameState, false);
-      this.state.start('GameState');
-      this.input.addMoveCallback((pointer, x, y) => {
-        // pointer is the active pointer, x and y give you the position of the pointer
-        // on the canvas so here you can position you custom cursor sprite
+    console.log("Constructor");
+    super(1280, 720, Phaser.CANVAS, 'content', {
+      create: () => { console.log("Create!"); this.create2(); },
+      preload: () => { console.log("Preload1"); this.preload(); },
+    });
+    this.state.add('GameState', GameState, false);
+    //this.state.start('GameState');
+    //this.input.addMoveCallback((pointer, x, y) => {
+      // pointer is the active pointer, x and y give you the position of the pointer
+      // on the canvas so here you can position you custom cursor sprite
 
-      });
-    }
+    //});
+  }
+
+  create2() {
+    console.log("Create...");
+    this.state.start('GameState');
+  }
+
+  preload() {
+    console.log("Preload...");
+    this.load.spritesheet('hammer', 'graphics/hammer.ss.png', 742, 600);
+  }
 }
 
 new Game();
--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,4 +1,5 @@
 import RainbowText from 'objects/RainbowText';
+import Hammer from 'objects/Hammer';
 
 class GameState extends Phaser.State {
 
@@ -6,6 +7,7 @@ class GameState extends Phaser.State {
     let center = { x: this.game.world.centerX, y: this.game.world.centerY }
     let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nFOOBAR!");
     text.anchor.set(0.5);
+    let hammer = new Hammer(this.game, center.x, center.y);
   }
 
 }
